c000f8d765822954597ec78050c16ddecbb1fe6e
WIS <38274782+WIS8@users.noreply.github.com>
2019-06-20 14:38:33 +0800

    solve conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
+import {RoomManagementComponent} from './leader/room-management/room-management.component';
+import {ApplicationCheckManagementComponent} from './leader/application-check-management/application-check-management.component';
+import {AccessManagementComponent} from './leader/access-management/access-management.component';
 import {ApplicationManagementComponent} from './staff/application-management/application-management.component';
 import {ManufacturerManagementComponent} from './staff/manufacturer-management/manufacturer-management.component';
 import {ModelManagementComponent} from './staff/model-management/model-management.component';
 import {StaffTopbarComponent} from './staff/staff-topbar/staff-topbar.component';
+import {LeaderTopbarComponent} from './leader/leader-topbar/leader-topbar.component';
 
 registerLocaleData(zh);
 
@@ -21,7 +26,11 @@ registerLocaleData(zh);
     ApplicationManagementComponent,
     ManufacturerManagementComponent,
     ModelManagementComponent,
-    StaffTopbarComponent
+    StaffTopbarComponent,
+    RoomManagementComponent,
+    ApplicationCheckManagementComponent,
+    AccessManagementComponent,
+    LeaderTopbarComponent
   ],
   imports: [
     BrowserModule,
@@ -30,6 +39,7 @@ registerLocaleData(zh);
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
