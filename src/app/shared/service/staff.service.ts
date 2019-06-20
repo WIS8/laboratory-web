dab759bcc862a6bbcbd2d238c6319fc9523990b5
WIS <38274782+WIS8@users.noreply.github.com>
2019-06-20 14:17:16 +0800

    delete error

--- src/app/shared/service/staff.service.ts
+++ src/app/shared/service/staff.service.ts
@@ -9,13 +9,17 @@ import {Observable} from 'rxjs';
 })
 export class StaffService {
 
-  private staffURL = `${environment.apiURL}/staff`;
+  private staffURL = `${environment.apiURL}/api/v1/staff`;
 
   constructor(private http: HttpClient) {
   }
 
   save(staff: Staff): Observable<Staff> {
     return this.http.post<Staff>(this.staffURL, staff);
+  }
+
+  findAllStaff(): Observable<Staff[]> {
+    return this.http.get<Staff[]>(this.staffURL);
   }
 
 }
